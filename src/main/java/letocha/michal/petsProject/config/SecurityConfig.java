ecce9f630bc616b7ce5a725e001439ff6d3cd797
Michał Łętocha <michalletocha6@gmail.com>
2023-06-29 17:46:47 +0200

    Saving Repo

--- src/main/java/letocha/michal/petsProject/config/SecurityConfig.java
+++ src/main/java/letocha/michal/petsProject/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package letocha.michal.petsProject.config;
 import letocha.michal.petsProject.service.SpringDataUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
--- src/main/java/letocha/michal/petsProject/repository/RoleRepository.java
+++ src/main/java/letocha/michal/petsProject/repository/RoleRepository.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Role findRoleByName(String name);
-//    List<Animal> findAnimalsByUser
 }
